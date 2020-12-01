8d9004a1668c51efe218faadb993623f95b45777
shenperson <wayne19990609@gmail.com>
2020-12-01 16:37:05 +0800

    main

--- python/src/main.py
+++ python/src/main.py
@@ -5,8 +5,12 @@ from paint import paint
 from style import *
 
 if __name__ == '__main__':
-    imgName = sys.argv[1]
-    saveName = sys.argv[2]
+    try:
+        imgName = sys.argv[1]
+        saveName = sys.argv[2]
+    except:
+        imgName = "../input/view.jpg"
+        saveName = "../output/view.jpg"
 
     """    Read image    """
     sourceImg = cv2.imread(imgName)  # nparray[width, height, (BGR)]
