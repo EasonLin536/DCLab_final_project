3bed4f0841fde9cdc6cdda8c5dabe6c33947687b
EasonLin536 <b06901153@ntu.edu.tw>
2020-12-04 17:11:52 +0800

    modify makeStroke as single cycle

--- python/src/makeStroke.py
+++ python/src/makeStroke.py
@@ -5,41 +5,41 @@ from estimate import *
 
 
 
-def makeStroke(R, x0, y0, refImage, canvas, gradX, gradY, gradM):
+def makeStroke(R, x0, y0, refImage, canvas, gradX, gradY, gradM, strokeLen, dxF, dyF):
     # x0, y0 : initial image coordinate
     # gradX, gradY : gradient direction
     # gradM : gradient magnitude
     
-    strokeColor = refImage[x0, y0]
+    # strokeColor = refImage[x0, y0]
     x = x0
     y = y0
-    dxF = 0  # final gradient change
-    dyF = 0
-    K = [[x, y]]  # points of stroke
+    # dxF = 0  # final gradient change
+    # dyF = 0
+    finish = False
 
-    for i in range(0, maxLen):  # stroke is limited by the maximum length
-        # coordinate must be within image dimensions
-        if x < 0 or y < 0 or x >= refImage.shape[0] or y >= refImage.shape[1]:
-            break
+    # for i in range(0, maxLen):  # stroke is limited by the maximum length
+    # coordinate must be within image dimensions
+    if x < 0 or y < 0 or x >= refImage.shape[0] or y >= refImage.shape[1]:
+        print("output of range")
 
-        refColor = refImage[x, y]  # refColor at coordinate
-        canvasColor = canvas[x, y]  # refColor of canvas
-        diffR = abs(int(refColor[0]) - int((canvasColor[0]))
-                    ) < abs(int(refColor[0]) - int(strokeColor[0]))
-        diffG = abs(int(refColor[1]) - int((canvasColor[1]))
-                    ) < abs(int(refColor[1]) - int(strokeColor[1]))
-        diffB = abs(int(refColor[2]) - int((canvasColor[2]))
-                    ) < abs(int(refColor[2]) - int(strokeColor[2]))
-        diffColor = (diffR and diffG and diffB)
+    refColor = refImage[x, y]  # refColor at coordinate
+    canvasColor = canvas[x, y]  # refColor of canvas
+    diffR = abs(int(refColor[0]) - int((canvasColor[0]))
+                ) < abs(int(refColor[0]) - int(strokeColor[0]))
+    diffG = abs(int(refColor[1]) - int((canvasColor[1]))
+                ) < abs(int(refColor[1]) - int(strokeColor[1]))
+    diffB = abs(int(refColor[2]) - int((canvasColor[2]))
+                ) < abs(int(refColor[2]) - int(strokeColor[2]))
+    diffColor = (diffR and diffG and diffB)
 
-        # returns stroke if refColor difference exceeded
-        if i > minLen and diffColor:
-            break
+    # returns stroke if refColor difference exceeded
+    if (strokeLen > minLen and diffColor) or gradM[x, y] == 0:
+        finish = True
+    else:
+    # if gradM[x, y] == 0:  # returns if gradient zero
+    #     finish = True
 
-        if gradM[x, y] == 0:  # returns if gradient zero
-            break
-
-        # normal gradient to stroke path
+    # normal gradient to stroke path
         dx = -gradY[x, y]
         dy = gradX[x, y]
         if (dxF * dx + dyF * dy < 0):  # ensures positve normal gradient
@@ -60,7 +60,9 @@ def makeStroke(R, x0, y0, refImage, canvas, gradX, gradY, gradM):
         y = math.floor(y + R * dy)
         dxF = dx
         dyF = dy
-        K.append([x, y])  # updates stroke points
+    # K.append([x, y])  # updates stroke points
 
-    return np.array(K), strokeColor
-    # return stroke, strokeColor
+    strokeLen += 1
+
+    return [x, y], dxF, dyF, strokeLen, finish
+    # return np.array(K), strokeColor
